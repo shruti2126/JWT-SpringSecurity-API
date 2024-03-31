99159c901616d8a18f8343f49b743382f856dd2c
Shruti <ss.sharma1826@gmail.com>
2024-03-31 13:58:34 +0530

    new branch, first commit

--- jwt-api/src/main/java/nielit/jwtapi/Entity/UserEntity.java
+++ jwt-api/src/main/java/nielit/jwtapi/Entity/UserEntity.java
@@ -1,18 +1,24 @@
 package nielit.jwtapi.Entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "users")
 public class UserEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	@Column(name = "username", unique = true, nullable = false)
 	private String username;
+	@Column(name = "password", nullable = false)
 	private String password;
-	private String roles;
+	@Column(name = "authority", nullable = false)
+	private String authority;
 }
